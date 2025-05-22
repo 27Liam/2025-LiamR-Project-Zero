74121be1c15d09ff582d05f1bb135a35806754a3
27Liam <114444535+27Liam@users.noreply.github.com>
2025-05-23 09:37:26 +1200

    Add files via upload

--- C_05_integer_checker_between_v2.py
+++ C_05_integer_checker_between_v2.py
@@ -1,5 +1,5 @@
 # Functions go here
-def num_check(question, low, high):
+def int_check(question, low, high):
     """Checks users enter an integer between 2 values"""
 
     error = f"Oops - please enter an integer between {low} and {high}"
@@ -25,16 +25,6 @@ while True:
 
     print()
 
-    # ask user for integer
-    my_num = int_check("Choose a number:", 1, 10)
-    print(f"You chose {my num}")
-
-    my_float = num_check("Please enter a number more than 0: ", "float")
-    print(f"Thanks. You chose {my_float}")
-    print()
-    my_int = num_check("Please enter an integer more than 0: ", "integer", "")
-
-    if my_int == "":
-        print("You have chosen infinite mode.")
-    else:
-        print(f"Thanks. You chose {my_int}")
+    # ask user for an integer
+    my_num = int_check("Choose a number: ", 1, 10)
+    print(f"You chose {my_num}")
